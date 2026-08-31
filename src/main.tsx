58ceed99e9c82c619901b554759943a3ee79d53c
cse <cseb.64.2022@gmail.com>
2026-08-31 19:38:22 +0530

    Fix application entry point

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RouterProvider } from "@tanstack/react-router";
+import { getRouter } from "./router";
+import "./styles.css";
+
+const router = getRouter();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <div>CAPMAP Loading...</div>
-  </React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 );
